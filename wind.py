1e1d0212c3b49e0e9ce9dd903cfbd53f75fd9035
Alyaksandr <6836808@gmail.com>
2015-09-16 16:49:26 +0300

    Forst commit

---
+++ wind.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'alex'
+from PyQt4 import QtCore, QtGui
+import sys
+
+app = QtGui.QApplication(sys.argv)
+window = QtGui.QWidget()
+window.setWindowTitle(u"Пepвaя nрограмма")
+window.resize(300, 70)
+label = QtGui.QLabel(u"<center>Привет !</center>")
+btnQuit = QtGui.QPushButton(u"&Зaкpьrrь")
+vbox = QtGui.QVBoxLayout()
+vbox.addWidget(label)
+vbox.addWidget(btnQuit)
+window .setLayout(vbox)
+QtCore.QObject.connect(btnQuit , QtCore.SIGNAL("clicked()"),
+                       QtGui.qApp, QtCore.SLOT("quit()"))
+window.show()
+sys.exit(app.exec_())
