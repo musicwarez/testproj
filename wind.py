d75bdb8b5a4e5d02e27b984bfec1b9b3012d8f36
Alyaksandr <6836808@gmail.com>
2015-09-16 17:00:17 +0300

    commit from pycharm

--- wind.py
+++ wind.py
@@ -6,7 +6,7 @@ import sys
 
 app = QtGui.QApplication(sys.argv)
 window = QtGui.QWidget()
-window.setWindowTitle(u"Пepвaя nрограмма")
+window.setWindowTitle(u"Это Пepвaя nрограмма")
 window.resize(300, 70)
 label = QtGui.QLabel(u"<center>Привет !</center>")
 btnQuit = QtGui.QPushButton(u"&Зaкpьrrь")
@@ -16,5 +16,6 @@ vbox.addWidget(btnQuit)
 window .setLayout(vbox)
 QtCore.QObject.connect(btnQuit , QtCore.SIGNAL("clicked()"),
                        QtGui.qApp, QtCore.SLOT("quit()"))
+
 window.show()
 sys.exit(app.exec_())
